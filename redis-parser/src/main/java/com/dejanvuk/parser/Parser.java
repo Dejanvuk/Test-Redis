22c3e82baff50db06f4891df73679f46b0a4dec7
Dejanvuk <dejan.vuk114@outlook.com>
2022-05-28 23:32:08 +0300

    Finished printMessage() utility

--- redis-parser/src/main/java/com/dejanvuk/parser/Parser.java
+++ redis-parser/src/main/java/com/dejanvuk/parser/Parser.java
@@ -303,8 +303,39 @@ public class Parser {
     ====================
     */
 
-    public void printMessage(Message message) {
+    /**
+     * Print the full command received
+     * @param messages
+     */
+    public void printMessage(List<Message> messages) {
+        StringBuilder sb = new StringBuilder();
+        boolean isMsg = false;
 
+        for(int i = 0; i < messages.size(); i++) { // 0 because first * is already skipped in readData
+            Message message = messages.get(i);
+
+            if(message.msgType != null) {
+                sb.append(message.msgType.toString() + "(");
+                isMsg = true;
+            }
+            else {
+                if(message.dataType == DataType.INTEGER) {
+                    sb.append(Integer.toString((Integer) message.data[0]));
+                }
+                else {
+                    sb.append((String) message.data[0]);
+                }
+
+                if(i != messages.size() - 1)
+                    sb.append(", ");
+            }
+        }
+
+        if(isMsg) {
+            sb.append(")");
+        }
+
+        System.out.println(sb.toString());
     }
 
     /**
@@ -333,4 +364,7 @@ public class Parser {
     Utility methods for parsing CLI input, for example messages
     ====================
     */
+
+
+
 }
