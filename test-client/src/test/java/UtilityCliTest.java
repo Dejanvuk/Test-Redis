412b5a1571493f2a0085e49aefd1e9071a64b876
Dejanvuk <dejan.vuk114@outlook.com>
2022-06-03 21:01:44 +0300

    added dependnecy plugin to the server

---
+++ test-client/src/test/java/UtilityCliTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UtilityCliTest {
+    @BeforeEach
+    public void setup() {
+        System.out.println("=============== Start of Parser Tests Setup ===============\n");
+    }
+
+    @Test
+    public void mockTest() {
+        System.out.println("test");
+    }
+}
