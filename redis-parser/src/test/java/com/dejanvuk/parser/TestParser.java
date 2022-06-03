89b41792d6721c9ab920ab4b6972fc8d5673b06b
Dejanvuk <dejan.vuk114@outlook.com>
2022-06-03 14:52:45 +0300

    finished readDataTestServer

--- redis-parser/src/test/java/com/dejanvuk/parser/TestParser.java
+++ redis-parser/src/test/java/com/dejanvuk/parser/TestParser.java
@@ -1,6 +1,7 @@
 package com.dejanvuk.parser;
 
 import com.dejanvuk.parser.types.DataType;
+import com.dejanvuk.parser.types.MsgType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -80,41 +81,80 @@ public class TestParser {
         List<Message> messages = new ArrayList<>();
         parser.readData(messages);
         // validate
+        List<Message> correctMessages = new ArrayList<>();
+        correctMessages.add(new Message.MessageBuilder().setMsgType(MsgType.SET).setDataType(DataType.BULK_STR).setLength(3).setData("SET").build());
+        correctMessages.add(new Message.MessageBuilder().setDataType(DataType.BULK_STR).setData("abcd").setLength(4).build());
+        correctMessages.add(new Message.MessageBuilder().setDataType(DataType.INTEGER).setData(123456).build());
+        assertEquals(messages, correctMessages);
 
         String _source2 = "*3\r\n$3\r\nSET\r\n$4\r\nabcd\r\n$7\r\nmessage\r\n";  // tests a set string command
         in.reset();
         d = new ByteArrayInputStream(_source2.getBytes("UTF-8"));
         messages = new ArrayList<>();
+        parser.setIn(new DataInputStream(d));
         parser.readData(messages);
         // validate
+        correctMessages = new ArrayList<>();
+        correctMessages.add(new Message.MessageBuilder().setMsgType(MsgType.SET).setDataType(DataType.BULK_STR).setLength(3).setData("SET").build());
+        correctMessages.add(new Message.MessageBuilder().setDataType(DataType.BULK_STR).setData("abcd").setLength(4).build());
+        correctMessages.add(new Message.MessageBuilder().setDataType(DataType.BULK_STR).setData("message").setLength(7).build());
+        assertEquals(messages, correctMessages);
+
 
         String _source3 = "*5\r\n$3\r\nSET\r\n$4\r\nabcd\r\n:1\r\n:22\r\n:333\r\n"; // tests a set array of integers command
         in.reset();
         d = new ByteArrayInputStream(_source3.getBytes("UTF-8"));
         messages = new ArrayList<>();
+        parser.setIn(new DataInputStream(d));
         parser.readData(messages);
         // validate
+        correctMessages = new ArrayList<>();
+        correctMessages.add(new Message.MessageBuilder().setMsgType(MsgType.SET).setDataType(DataType.BULK_STR).setLength(3).setData("SET").build());
+        correctMessages.add(new Message.MessageBuilder().setDataType(DataType.BULK_STR).setData("abcd").setLength(4).build());
+        correctMessages.add(new Message.MessageBuilder().setDataType(DataType.INTEGER).setData(1).build());
+        correctMessages.add(new Message.MessageBuilder().setDataType(DataType.INTEGER).setData(22).build());
+        correctMessages.add(new Message.MessageBuilder().setDataType(DataType.INTEGER).setData(333).build());
+        assertEquals(messages, correctMessages);
 
-        String _source4 = "*2\r\n$3\r\nGET\r\n$3\r\nabc\r\n"; // tests a get command
+
+        String _source4 = "*2\r\n$3\r\nGET\r\n$4\r\nabcd\r\n"; // tests a get command
         in.reset();
         d = new ByteArrayInputStream(_source4.getBytes("UTF-8"));
         messages = new ArrayList<>();
+        parser.setIn(new DataInputStream(d));
         parser.readData(messages);
         // validate
+        correctMessages = new ArrayList<>();
+        correctMessages.add(new Message.MessageBuilder().setMsgType(MsgType.GET).setDataType(DataType.BULK_STR).setLength(3).setData("GET").build());
+        correctMessages.add(new Message.MessageBuilder().setDataType(DataType.BULK_STR).setData("abcd").setLength(4).build());
+        assertEquals(messages, correctMessages);
 
-        String _source5 = "*2\r\n$6\r\nDELETE\r\n$3\r\nabc\r\n"; // tests a delete command
+
+        String _source5 = "*2\r\n$6\r\nDELETE\r\n$4\r\nabcd\r\n"; // tests a delete command
         in.reset();
         d = new ByteArrayInputStream(_source5.getBytes("UTF-8"));
         messages = new ArrayList<>();
+        parser.setIn(new DataInputStream(d));
         parser.readData(messages);
         // validate
+        correctMessages = new ArrayList<>();
+        correctMessages.add(new Message.MessageBuilder().setMsgType(MsgType.DELETE).setDataType(DataType.BULK_STR).setLength(6).setData("DELETE").build());
+        correctMessages.add(new Message.MessageBuilder().setDataType(DataType.BULK_STR).setData("abcd").setLength(4).build());
+        assertEquals(messages, correctMessages);
+
 
         String _source6 = "*3\r\n$6\r\nRENAME\r\n$6\r\noldKey\r\n$6\r\nnewKey\r\n"; // tests a rename command
         in.reset();
         d = new ByteArrayInputStream(_source6.getBytes("UTF-8"));
         messages = new ArrayList<>();
+        parser.setIn(new DataInputStream(d));
         parser.readData(messages);
         // validate
+        correctMessages = new ArrayList<>();
+        correctMessages.add(new Message.MessageBuilder().setMsgType(MsgType.RENAME).setDataType(DataType.BULK_STR).setLength(6).setData("RENAME").build());
+        correctMessages.add(new Message.MessageBuilder().setDataType(DataType.BULK_STR).setData("oldKey").setLength(6).build());
+        correctMessages.add(new Message.MessageBuilder().setDataType(DataType.BULK_STR).setData("newKey").setLength(6).build());
+        assertEquals(messages, correctMessages);
 
         System.out.println("blabla");
     }
